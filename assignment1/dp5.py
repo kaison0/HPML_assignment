ab5a3d8f4dcf66840546102ad5810184bab8388f
Kai <ykmao1515@outlook.com>
2025-09-22 05:08:07 +0000

    fix: dp4 and dp5

--- assignment1/dp5.py
+++ assignment1/dp5.py
@@ -1,5 +1,6 @@
 import time
 import numpy as np
+import argparse
 
 def dp(N, A, B):
     R = np.dot(A, B)
@@ -10,22 +11,29 @@ def evaluate(N, reps):
     B = np.ones(N, dtype = np.float32)
 
     average_time = 0.0
+    result = 0.0
     for i in range(0, reps):
         t1 = time.clock_gettime(time.CLOCK_MONOTONIC)
 
-        dp(N, A, B)
+        result = dp(N, A, B)
 
         t2 = time.clock_gettime(time.CLOCK_MONOTONIC)
 
         elapsed = t2 - t1
         if (i >= reps / 2):
             average_time = average_time + elapsed / (reps / 2)
+
+    print(f"result: {result}")        
     bandwith = 8.0 * N / average_time / 1e9
     throughput = 2.0 * N / average_time
     print(f"N: {N} <T>: {average_time} sec B: {bandwith} GB/sec F: {throughput} FLOP/sec")
 
 
 if __name__ == '__main__':
-    evaluate(1000000, 1000)
-    evaluate(300000000, 20)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("N", type=int, help="N")
+    parser.add_argument("reps", type=int, help="reps")
+    args = parser.parse_args()
+
+    evaluate(args.N, args.reps)
    # evaluate(20, 20)
