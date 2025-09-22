053b53314376cac228d5cfcc66e8a65b1197690a
Kai <ykmao1515@outlook.com>
2025-09-22 01:02:19 +0000

    Add: dp3 dp4 dp5

---
+++ assignment1/dp3.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mkl_cblas.h>
+
+void init(long N, float **pA, float **pB) {
+	*pA = malloc(N * sizeof(float));
+	*pB = malloc(N * sizeof(float));
+
+	srand(time(NULL));
+	for (int i = 0; i < N; i++) {
+		(*pA)[i] = 1.0f;
+		(*pB)[i] = 1.0f;
+	}
+
+}
+float dp(long N, float *pA, float *pB) {
+	float R = 0.0;
+	int j;
+	for (j = 0; j < N; j++) 
+		R += pA[j] * pB[j];
+     
+	return R;
+}
+
+float dpunroll(long N, float *pA, float *pB) {
+    float R = 0.0f;
+    int j;
+
+    for (j = 0; j < N; j += 4)
+        R += pA[j] * pB[j] + pA[j + 1] * pB[j + 1] 
+            + pA[j + 2] * pB[j + 2] + pA[j + 3] * pB[j + 3];
+
+    return R;
+}
+float bdp(long N, float *pA, float *pB) {
+    float R = cblas_sdot(N, pA, 1, pB, 1);
+    
+    return R;
+}
+
+void evaluate(int N, int reps) {
+    float *pA, *pB;
+
+	init(N, &pA, &pB);
+
+    float average_exe_time = 0.0f;
+
+    struct timespec start, end;
+
+
+    for (int i = 0; i < reps; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &start);
+
+	    //dp(N, pA, pB);
+        //dpunroll(N, pA, pB);
+        bdp(N, pA, pB);
+
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        
+        double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+        if (i >= reps / 2) {
+            average_exe_time += elapsed / (reps / 2);     
+        }
+    }
+    float bandwidth = (float)8 * N / average_exe_time / 1e9;// GB/sec
+    float throughput = (float)(2  * N) / average_exe_time;// FLOP/sec
+
+    printf("N %d <T>: %f sec B: %f GB/sec F: %f FLOP/sec \n", N, average_exe_time, bandwidth, throughput);
+
+	free(pA); free(pB);
+}
+
+int main() {
+	float *pA, *pB;
+	
+	evaluate(1000000, 1000);
+    evaluate(300000000, 20);
+
+}
---
+++ assignment1/dp5.py
@@ -0,0 +1,31 @@
+import time
+import numpy as np
+
+def dp(N, A, B):
+    R = np.dot(A, B)
+    return R
+
+def evaluate(N, reps):   
+    A = np.ones(N, dtype = np.float32)
+    B = np.ones(N, dtype = np.float32)
+
+    average_time = 0.0
+    for i in range(0, reps):
+        t1 = time.clock_gettime(time.CLOCK_MONOTONIC)
+
+        dp(N, A, B)
+
+        t2 = time.clock_gettime(time.CLOCK_MONOTONIC)
+
+        elapsed = t2 - t1
+        if (i >= reps / 2):
+            average_time = average_time + elapsed / (reps / 2)
+    bandwith = 8.0 * N / average_time / 1e9
+    throughput = 2.0 * N / average_time
+    print(f"N: {N} <T>: {average_time} sec B: {bandwith} GB/sec F: {throughput} FLOP/sec")
+
+
+if __name__ == '__main__':
+    evaluate(1000000, 1000)
+    evaluate(300000000, 20)
+   # evaluate(20, 20)
